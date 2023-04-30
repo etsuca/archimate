671805b2e7a7c5a3e862e0599dd19353c379e70c
tuks34 <ebiharatsukasa@gmail.com>
2023-04-30 13:16:06 +0900

    fix: ビューとフラッシュメッセージの調整

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,9 +7,9 @@ class ProfilesController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to profile_path, success: t('defaults.message.updated', item: User.model_name.human)
+      redirect_to profile_path, notice: t('defaults.message.updated', item: User.model_name.human)
     else
-      flash.now['danger'] = t('defaults.message.not_updated', item: User.model_name.human)
+      flash.now['notice'] = t('defaults.message.not_updated', item: User.model_name.human)
       render :edit
     end
   end
