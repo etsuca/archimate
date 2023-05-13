ce7f4f761e512e60f0c213cf4744e0e8fd11e9ae
tuks34 <ebiharatsukasa@gmail.com>
2023-05-14 01:39:54 +0900

    fix: トップページを建築のランダム表示ページに変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,9 @@ class StaticPagesController < ApplicationController
   skip_before_action :require_login, only: %i[welcome]
 
   def top
+    user_liked_architecture_ids = Like.where(user_id: current_user.id).pluck(:architecture_id)
+    architecture = Architecture.where.not(id: user_liked_architecture_ids).where(open_range: 'publish')
+    @architecture = architecture.not_by(current_user).offset( rand(architecture.not_by(current_user).count) ).first
   end
 
   def welcome
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create]
   resources :architecture do
     collection do
-      get :random
       get :likes
     end
   end
