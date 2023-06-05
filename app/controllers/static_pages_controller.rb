c46c456016a83f3dab0bab674d33675937875607
tuks34 <ebiharatsukasa@gmail.com>
2023-06-05 16:00:56 +0900

    refactor: リファクタリング

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,9 @@ class StaticPagesController < ApplicationController
   skip_before_action :require_login, only: %i[welcome]
 
   def top
+    others_architecture = Architecture.not_by(current_user)
     user_liked_architecture_ids = Like.where(user_id: current_user.id).pluck(:architecture_id)
-    architecture = Architecture.where.not(id: user_liked_architecture_ids).where(open_range: 'publish')
-    @architecture = architecture.not_by(current_user).offset( rand(architecture.not_by(current_user).count) ).first
+    @architecture = others_architecture.where.not(id: user_liked_architecture_ids).where(open_range: 'publish').offset( rand(others_architecture.count) ).first
   end
 
   def welcome
