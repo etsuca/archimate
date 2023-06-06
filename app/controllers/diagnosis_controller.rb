e6fffd52e6359563ad9b417e83d109b94229ebeb
tuks34 <ebiharatsukasa@gmail.com>
2023-06-06 20:25:58 +0900

    fix: 軽微な修正

--- app/controllers/diagnosis_controller.rb
+++ app/controllers/diagnosis_controller.rb
@@ -5,14 +5,14 @@ class DiagnosisController < ApplicationController
   end
 
   def index
-    @others_architecture = Architecture.where.not(user_id: current_user&.id).where(experience: 0)
+    others_architecture = Architecture.where.not(user_id: current_user&.id).where(experience: 0)
     selected_tag_ids = params[:answer]
     @match_tag_count = [0]
     tmp_match_tag_count = 0
     @matched_architecture = []
     @all_tmp_match_tag_count = []
 
-    @others_architecture.each do |architecture|
+    others_architecture.each do |architecture|
       tmp_match_tag_count = 0
       architecture.tags.each do |tag|
         if selected_tag_ids&.include?(tag.id.to_s)
@@ -29,6 +29,9 @@ class DiagnosisController < ApplicationController
           @matched_architecture&.delete(@matched_architecture.min)
         end
       end
+    end
+    if @matched_architecture.length < 3
+      redirect_to new_diagnosis_path, notice: 'マッチする建築がありませんでした。もう一度好みを教えてください。'
     end
   end
 end
