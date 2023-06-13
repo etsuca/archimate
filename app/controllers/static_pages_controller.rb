250f8bbe21aa9f1554aae29624d8809a1ab0cbdf
tuks34 <ebiharatsukasa@gmail.com>
2023-06-13 19:36:26 +0900

    refactor: 質問・タグ・背景画像を追加・整理

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
     others_architecture = Architecture.not_by(current_user)
     user_liked_architecture_ids = Like.where(user_id: current_user.id).pluck(:architecture_id)
     @architecture = others_architecture.where.not(id: user_liked_architecture_ids).where(open_range: 'publish').shuffle.first
-    @images = @architecture.images.to_json.html_safe
+    @images = @architecture&.images.to_json.html_safe
   end
 
   def welcome
