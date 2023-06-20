ad4c4ca846199b0e2740219750c8a0d0c0de857e
tuks34 <ebiharatsukasa@gmail.com>
2023-06-20 13:34:06 +0900

    feat: 建築記録フォームに画像プレビュー機能を追加

--- app/javascript/javascripts/shared/diagnosis_progress_bar.js
+++ app/javascript/javascripts/shared/diagnosis_progress_bar.js
@@ -12,6 +12,11 @@ const initializeArchitecture = () => {
     const progressBar = container.querySelector('.progress-bar');
     let currentIndex = 0;
 
+    const updateProgressBar = () => {
+      const progressBarWidth = (currentIndex + 1) * (100 / architectureImages.length);
+      progressBar.style.width = `${progressBarWidth}%`;
+    }
+    
     updateProgressBar();
 
     image.addEventListener('click', (event) => {
@@ -23,14 +28,10 @@ const initializeArchitecture = () => {
       } else {
         currentIndex = (currentIndex - 1 + architectureImages.length) % architectureImages.length;
       }
-
-      image.src = architectureImages[currentIndex].url;
+      if (architectureImages.length >= 2) {
+        image.src = architectureImages[currentIndex];
+      }  
       updateProgressBar();
     });
-
-    function updateProgressBar() {
-      const progressBarWidth = (currentIndex + 1) * (100 / architectureImages.length);
-      progressBar.style.width = `${progressBarWidth}%`;
-    }
   });
 };
