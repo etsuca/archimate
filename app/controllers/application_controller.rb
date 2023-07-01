7bfe6715f40c1402a23ff1b459dc4f2248ed1d3c
tuks34 <ebiharatsukasa@gmail.com>
2023-07-01 19:09:21 +0900

    feat: ログイン、新規登録、パスワード再登録ページを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_user, :user_signed_in?
+
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :avatar_cache])
+  end
 end
