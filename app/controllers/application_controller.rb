41906032504aae2e05f4a49923636af528857fd6
tuks34 <ebiharatsukasa@gmail.com>
2023-07-01 12:51:22 +0900

    fix: authenticate_user!をログインが必要なコントローラーに追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_user, :user_signed_in?
-
-  private
-
-  def authenticate_user!
-    redirect_to welcome_path unless session[:user_id]
-  end
-
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find_by(uid: session[:user_id])
-  end
-
-  def user_signed_in?
-    !!session[:user_id]
-  end
 end
--- app/controllers/diagnosis_controller.rb
+++ app/controllers/diagnosis_controller.rb
@@ -1,5 +1,4 @@
 class DiagnosisController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[new index]
 
   def new
     questions = [
