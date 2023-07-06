63f4df4c01d9c8e3de0907425f70897e6df71828
tuks34 <ebiharatsukasa@gmail.com>
2023-07-06 18:17:01 +0900

    fix: 新規登録時のメールアドレス認証をオフ、パスワードの英数組み合わせ必須のバリデーションをオフに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,10 @@ class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable,
-        :confirmable, :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter]
+        :lockable, :timeoutable, :omniauthable, omniauth_providers: [:twitter]
   
   validates :name, presence: true, length: { maximum: 255 }, on: :create
-  validate :password_complexity
-  def password_complexity
-    return if password.blank? || password =~ /(?=.*?[a-z])(?=.*?[0-9])/
-    errors.add :password, "は数字と英字を混ぜたものを入力してください"
-  end
+  # validate :password_complexity
   has_many :architecture, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_architecture, through: :likes, source: :architecture
@@ -46,9 +42,14 @@ class User < ApplicationRecord
     like_architecture.include?(architecture)
   end
 
-    private
+  private
+  
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
 
-    def self.dummy_email(auth)
-      "#{auth.uid}-#{auth.provider}@example.com"
-    end
+  def password_complexity
+    return if password.blank? || password =~ /(?=.*?[a-z])(?=.*?[0-9])/
+    errors.add :password, "は数字と英字を混ぜたものを入力してください"
+  end
 end
