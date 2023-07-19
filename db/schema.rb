ac3e4dcd1cac41be9bd5d8a58cdcbaaae8948ba8
tuks34 <ebiharatsukasa@gmail.com>
2023-07-20 00:21:33 +0900

    fix: prefカラムにNOT NULL制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_16_053501) do
+ActiveRecord::Schema.define(version: 2023_07_17_150912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2023_07_16_053501) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "pref"
+    t.string "pref", null: false
     t.index ["user_id"], name: "index_architecture_on_user_id"
   end
 
