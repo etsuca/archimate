d873ed9bc4c0fd8f12f8e5d1dc220f0f1a95ce97
tuks34 <ebiharatsukasa@gmail.com>
2023-08-30 19:07:15 +0900

    fix: rubocop

--- app/controllers/architecture_controller.rb
+++ app/controllers/architecture_controller.rb
@@ -6,12 +6,8 @@ class ArchitectureController < ApplicationController
   def index
     @architecture = @base_query.distinct.order(created_at: :desc).page(params[:page])
     respond_to do |format|
-      format.html do
-      end
-
-      format.js do
-        render 'index.js.erb', formats: [:js], handlers: [:erb]
-      end
+      format.html
+      format.js { render 'index.js.erb', formats: [:js], handlers: [:erb] }
     end
   end
 
--- app/controllers/concerns/base_query_concern.rb
+++ app/controllers/concerns/base_query_concern.rb
@@ -12,24 +12,22 @@ module BaseQueryConcern
     selected_pref = params[:pref]
     keyword = params[:keyword]
 
-    if params[:category] == 'others_architecture'
-      @base_query = Architecture.not_by(current_user)
-    elsif params[:category] == 'liked_architecture'
-      @base_query = current_user.like_architecture
-    else
-      @base_query = Architecture.where(user_id: current_user.id)
-    end
+    @base_query = if params[:category] == 'others_architecture'
+                    Architecture.not_by(current_user)
+                  elsif params[:category] == 'liked_architecture'
+                    current_user.like_architecture
+                  else
+                    Architecture.where(user_id: current_user.id)
+                  end
 
     if selected_tag_ids.present?
       @base_query = @base_query.joins(:tags).where(tags: { id: selected_tag_ids.first })
-      selected_tag_ids[1..-1].each do |tag_id|
+      selected_tag_ids[1..].each do |tag_id|
         @base_query = @base_query.where(id: Architecture.joins(:tags).where(tags: { id: tag_id }))
       end
     end
 
-    if selected_pref.present?
-      @base_query = @base_query.where(pref: selected_pref)
-    end
+    @base_query = @base_query.where(pref: selected_pref) if selected_pref.present?
 
     if keyword.present?
       keywords = keyword.split
