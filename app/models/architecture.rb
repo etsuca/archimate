7e94d4d45c3e467b983392b03516adf27bc4f940
tuks34 <ebiharatsukasa@gmail.com>
2023-09-25 01:02:24 +0900

    fix: 無駄なバリデーションを削除

--- app/models/architecture.rb
+++ app/models/architecture.rb
@@ -11,10 +11,7 @@ class Architecture < ApplicationRecord
   validates :architect, length: { maximum: 255 }
   validates :architect, format: { without: /[!@#\$%\^&\*\\?{}:"<>|¥;']/, message: 'に使用できない文字が含まれています' }
   validates :description, length: { maximum: 65_535 }
-  validates :images, attached_file_presence: true
   validates :images, attached_file_number: { maximum: 10 }
-  validates :images, attached_file_size: { maximum: 10.megabytes }
-  validates :images, attached_file_type: { pattern: %r{^image/(jpeg|png|tiff|heic|heif)$} }
 
   enum open_range: { unpublish: 0, publish: 1 }
   enum experience: { possible: 0, impossible: 1 }
