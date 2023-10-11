c110fe85a052867bf842dd0161ac3d14e1a90869
tuks34 <ebiharatsukasa@gmail.com>
2023-10-11 15:52:15 +0900

    feat: タグ検索を非同期処理に変更

--- app/javascript/packs/architecture/tag.js
+++ app/javascript/packs/architecture/tag.js
@@ -1,13 +1,13 @@
 document.addEventListener('turbolinks:load', () => {
   const tagButtons = document.querySelectorAll('.tag-button');
+  const gridElement = document.querySelector('.grid');
+  const paginationContainer = document.querySelector('.pagination'); // ページネーションを表示するコンテナ
 
   tagButtons.forEach((button) => {
     button.addEventListener('click', () => {
       const checkbox = button.previousElementSibling;
-      const form = document.querySelector('form');
-
       checkbox.checked = !checkbox.checked;
-
+      saveSelectedTagsToLocalStorage();
       if (checkbox.checked) {
         button.classList.remove('text-gray-600');
         button.classList.add('bg-fifth', 'text-white');
@@ -16,26 +16,67 @@ document.addEventListener('turbolinks:load', () => {
         button.classList.add('text-gray-600');
       }
 
-      const selectedTagIds = Array.from(document.querySelectorAll('input[name="tag_ids[]"]:checked'))
-        .map((checkbox) => checkbox.value);
+      const selectedTagIds = Array.from(document.querySelectorAll('input[name="tag_ids[]"]:checked')).map((checkbox) => checkbox.value);
 
-      form.submit();
+      const currentUrl = new URL(window.location.href);
+      const existingParams = currentUrl.searchParams;
+
+      // 現在のクエリパラメータからkeyword, pref, categoryを取得
+      const currentKeyword = existingParams.get('keyword');
+      const currentPref = existingParams.get('pref');
+      const currentCategory = existingParams.get('category');
+
+      // 新しいクエリパラメータを作成
+      const newParams = new URLSearchParams();
+      newParams.set('keyword', currentKeyword || ''); // keywordを追加、存在しなければ空文字列
+      newParams.set('pref', currentPref || ''); // prefを追加、存在しなければ空文字列
+      newParams.set('category', currentCategory || ''); // categoryを追加、存在しなければ空文字列
+
+      if (selectedTagIds.length > 0) {
+        selectedTagIds.forEach((tagId) => {
+          newParams.append('tag_ids[]', tagId);
+        });
+      }
+
+      const queryParams = newParams.toString();
+
+      // リクエストを送信
+      fetch(`/architecture?${queryParams}`, {
+        method: 'GET',
+      })
+      .then((response) => response.text())
+      .then((data) => {
+        const resultContainer = document.createElement('div');
+        resultContainer.innerHTML = data;
+        const newResults = resultContainer.querySelector('.grid');
+        const newPagination = resultContainer.querySelector('.pagination'); // 新しいページネーション
+
+        gridElement.innerHTML = newResults.innerHTML;
+
+        if (newPagination) {
+          paginationContainer.innerHTML = newPagination.innerHTML;
+        } else {
+          // 新しいページネーションが存在しない場合はクリア
+          paginationContainer.innerHTML = '';
+        }
+        
+        // タグを選択または選択解除した後、クエリパラメータにpageが存在する場合はpageを削除
+        const currentUrl = new URL(window.location.href);
+        const existingParams = currentUrl.searchParams;
+        if (existingParams.has('page')) {
+          existingParams.delete('page');
+          history.replaceState(null, null, `?${existingParams.toString()}`);
+        }
+      })
+      .catch((error) => {
+        console.error('Ajaxリクエストエラー:', error);
+      });
     });
   });
 
-  // ページロード時に選択されたタグIDをクエリパラメータから取得し、チェックボックスを選択状態にする
-  const urlParams = new URLSearchParams(window.location.search);
-  const selectedTagIds = urlParams.getAll('tag_ids[]');
-  const tagCheckboxes = document.querySelectorAll('input[name="tag_ids[]"]');
-
-  tagCheckboxes.forEach((checkbox) => {
-    const tagId = checkbox.value;
-    checkbox.checked = selectedTagIds.includes(tagId);
-
-    if (checkbox.checked) {
-      const button = checkbox.nextElementSibling;
-      button.classList.remove('text-gray-600');
-      button.classList.add('bg-fifth', 'text-white');
-    }
-  });
+  const saveSelectedTagsToLocalStorage = () => {
+    const selectedTagIds = Array.from(document.querySelectorAll('input[name="tag_ids[]"]:checked')).map((checkbox) => checkbox.value);
+    // キャッシュに選択されたタグ情報を保存
+    localStorage.setItem('selectedTags', JSON.stringify(selectedTagIds));
+  }
 });
