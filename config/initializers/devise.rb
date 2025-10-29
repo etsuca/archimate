28eb421927ab8a5321a5ecbcac7a4eca76dab72a
ebiharatsukasa <ebiharatsukasa@gmail.com>
2025-10-30 02:02:38 +0900

    fix: omniauthの環境変数は本番環境ではENVを使うように修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,10 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:secret_id], {
+  client_id     = ENV['GOOGLE_CLIENT_ID']     || Rails.application.credentials.google[:client_id]
+  client_secret = ENV['GOOGLE_CLIENT_SECRET'] || Rails.application.credentials.google[:client_secret]
+
+  config.omniauth :google_oauth2, client_id, client_secret, {
     access_type: 'offline',
     prompt: 'consent',
     scope: 'email'
