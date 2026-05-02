8bfa71475ef80eb5de3514402b4739391c3f560a
tsukasaebihara <ebiharatsukasa@gmail.com>
2026-05-02 09:29:05 +0900

    マイページのビューとコントローラーの責務を分離

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,7 @@ class UsersController < ApplicationController
     @buildings_count = current_user.buildings.count
     @liked_buildings_count = current_user.liked_buildings.count
     @received_likes_count = Like.where(building_id: current_user.buildings.select(:id)).count
-    first_building = current_user.buildings.order(:created_at).first
-    @first_building_created_at = l(first_building.created_at) if first_building
+    @first_building_created_at = current_user.buildings.order(:created_at).first&.created_at
     @visited_prefecture_count = current_user.buildings.map { |a| a.pref }.uniq.count
     @prefecture_completion_percentage = (@visited_prefecture_count / 47.00 * 100).floor
   end
